29d43daa4f189b8a26863e365a8c180293685531
CHAITHANYA-226 <chaithanyan226@gmail.com>
2025-03-05 23:21:46 +0530

    Add files via upload

---
+++ number_postive_or_negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("ente a number");
+    scanf("%d",&n);
+    if(n>=0){
+        printf("number is positive");
+    }      
+    else{
+        printf("number is negative");
+    }
+    return 0;
+}
